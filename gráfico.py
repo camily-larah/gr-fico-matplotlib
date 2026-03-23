1a207b29389f2e6c6f34ccb66d09cc0fa1474c75
camily-larah <santos.jesus.camily@escola.pr.gov.br>
2026-03-23 10:38:44 +0000

    criação grafico

--- gráfico.py
+++ gráfico.py
@@ -1,1 +1,8 @@
-
+import matplotlib.pyplot as plt
+meses = ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho']
+vendas = [200, 300, 340, 305, 360, 400]
+plt.plot(meses, vendas)
+plt.title('Vendas no primeiro semestre')
+plt.xlabel('Meses')
+plt.ylabel('Vendas')
+plt.savefig("grafico_barras.png")
